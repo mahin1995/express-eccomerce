29c18311e24d79e705f5a312809420f304b649f4
mahin1995 <mohimanul8@gmail.com>
2022-01-06 00:35:56 +0600

    f

--- server.js
+++ server.js
@@ -11,5 +11,14 @@ app.get("/", (req, res) => {
 app.get("/product_details", (req, res) => {
     res.sendFile(path.resolve(__dirname, "frontend", "product_details.html"));
 });
+app.get("/cart", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "cart.html"));
+});
+app.get("/products", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "products.html"));
+});
+app.get("/account", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "account.html"));
+});
 
-app.listen(3000, () => console.log("Server running..."));
+app.listen(3030, () => console.log("Server running..."));
